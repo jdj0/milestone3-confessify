25c91c2f77c8eaac61bbe829fa1d1015524f53d2
jdj0 <joshua.davidjones92@gmail.com>
2022-07-12 12:54:32 +0000

    Add register.html template and register form

--- confessify/routes.py
+++ confessify/routes.py
@@ -1,8 +1,19 @@
 from flask import render_template
 from confessify import app, db
 from confessify.models import User, Post
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/confess")
+def confess():
+    return render_template("confess.html")
