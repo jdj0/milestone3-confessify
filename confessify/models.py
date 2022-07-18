4a518c01b5d0bb76247f356e6f9135714282880e
jdj0 <joshua.davidjones92@gmail.com>
2022-07-18 13:14:17 +0000

    Alter User table schema by removing email column

--- confessify/models.py
+++ confessify/models.py
@@ -4,7 +4,6 @@ from confessify import db
 class User(db.Model):
     # schema for user account
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(75), unique=True, nullable=False)
     password = db.Column(db.String(25), nullable=False)
     username = db.Column(db.String(25), unique=True, nullable=False)
     posts = db.relationship("Post", backref="user", cascade="all, delete", lazy=True)
