0747ea48eb0a99cf74b50be9f5df4c7a33696759
jdj0 <joshua.davidjones92@gmail.com>
2022-08-05 16:22:25 +0000

    Add delete functionality to posts

--- confessify/routes.py
+++ confessify/routes.py
@@ -84,6 +84,14 @@ def edit_post(post_id):
     return render_template("edit_post.html", post=post)
 
 
+@app.route("/delete_post/<int:post_id>")
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for("posts"))
+
+
 @app.route("/logout")
 def logout():
     session.pop("user", None)
